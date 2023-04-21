273a5b508cd27f893227d12d2bc9d4d855f51991
Azaruddin8297 <ajarpopo.com>
2023-04-21 09:23:35 +0530

    Invoice

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+            double fare = invoiceGenerator.CalculateFare(5.0, 10);
+
+            Console.WriteLine(fare);
+        }
+    }
+}
